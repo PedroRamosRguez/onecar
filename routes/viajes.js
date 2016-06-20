ee37333114ad1576d98f1d6b742d45db2cb68911
alu0100505078 <alu0100505078@ull.edu.es>
2016-06-20 10:33:36 +0100

    Terminado controlador de viajes

--- routes/viajes.js
+++ routes/viajes.js
@@ -20,37 +20,22 @@ connection.connect((err)=>{
 });
 
 //peticion post del login a la platforma.
-  router.post('/',(req,res)=>{
+  router.get('/',(req,res)=>{
   console.log("estoy en el controlador de viajes...");
-     let destino = req.body.destino.toUpperCase(),
-         data = {},
-	 array = [], 
- 	 valido=0;
-     //console.log("variable  usuario: "+usuario+", variable pass: "+pass);
-     connection.query('SELECT *  FROM Usuarios ',(err,rows,fields)=>{
-        if(err){
-	   throw err;
-        }else{
-	  for (var i=0;i<rows.length;i++){
-	    //console.log(rows[i]);
-	    if(rows[i].Zona === destino && rows[i].Tipo_Usuario === "CONDUCTOR"){
-		//res.redirect(200,'/');//cambiarlo para que dirija a la pagina en banot principal...			
-	   	console.log("coincidencia...");
-		data["Usuarios"] = rows[i];
-		array.push(rows[i]);
-		console.log(data); 
-		valido=1;	
-	    }
-         } 
-	}
-        if(valido === 0){   
-          console.log("no se encontraron coincidencias");
-          res.status(500).send('Error de busqueda de destinos');
-          //res.redirect('/'); modificar para que luego envie a la pagina del logueo de bannot
-	}else{
-	 res.status(200).jsonp(array);
-	}	 
-     });
+  connection.query('SELECT * FROM Usuarios',(err,rows,fields)=>{
+    let data={};
+    if(err){
+      console.log("error al conectar a la base de datos..");
+      res.status(500).send('Error al conectar a la base de datos');
+      throw err;
+    }
+    if(rows.length!=0){
+      data["Usuarios"]=rows;
+      res.status(200).jsonp(data);
+    }else{
+      res.status(500).send('base de datos vacía');
+    }
   });
+});
 module.exports = router;
 })();
